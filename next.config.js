1759084ebbfda800ddc1eccadfec18817c080df7
filipbosnjak <filipo.bosnjak@gmail.com>
2021-08-20 16:05:25 +0200

    Webpack update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-}
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
