53063e36a98eed1b25aeece10a34c9ea67144e6a
filipbosnjak <filipo.bosnjak@gmail.com>
2021-08-20 16:46:20 +0200

    Head error

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,59 +2,56 @@ import React from "react";
 //File system - runs on a server (node)
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter"
-import Head from "next/Head"
-import marked from "marked"
-import Link from "next/Link"
+import matter from "gray-matter";
+import Head from "next/head";
+import marked from "marked";
+import Link from "next/link";
 
-
-
-const Post = ({content, data, html}) => {
+const Post = ({ content, data, html }) => {
   return (
     <>
-    <Head>
-      <title>{data.title}</title>
-      <meta title="description" content={data.description}/>      
-    </Head>
-    <body>
-    <h1>dangerouslySetInnerHTML: </h1>
-    <div dangerouslySetInnerHTML={{__html:content}}></div>
-    <Link href="/">Go back</Link>
-    </body>
-    
+      <Head>
+        <title>{data.title}</title>
+        <meta title='description' content={data.description} />
+      </Head>
+      <body>
+        <h1>dangerouslySetInnerHTML: </h1>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        <Link href='/'>Go back</Link>
+      </body>
     </>
-  )
-}
+  );
+};
 
 export const getStaticPaths = async () => {
+  const files = fs.readdirSync("posts");
 
-  const files = fs.readdirSync('posts');
-
-  const paths = files.map( filename => ({
+  const paths = files.map((filename) => ({
     params: {
-      slug: filename.replace('.md' , '').replace('.html' , '')
-    }})
-  )
+      slug: filename.replace(".md", "").replace(".html", ""),
+    },
+  }));
   return {
-    paths ,
-    fallback: false
-  }
-}
+    paths,
+    fallback: false,
+  };
+};
 
 //Function returning props to the post component - data feeding
-export const getStaticProps = async ({params: {slug}}) => {
-
+export const getStaticProps = async ({ params: { slug } }) => {
   //path.join - handles / and \ in the path
-  const markdownWithMetaData = fs.readFileSync(path.join("posts", slug + ".html")).toString();
+  const markdownWithMetaData = fs
+    .readFileSync(path.join("posts", slug + ".html"))
+    .toString();
 
-  const parsedMarkdown = matter(markdownWithMetaData)
+  const parsedMarkdown = matter(markdownWithMetaData);
   return {
     props: {
       content: parsedMarkdown.content,
       data: parsedMarkdown.data,
-      html:markdownWithMetaData
-    }
-  }
-}
- 
+      html: markdownWithMetaData,
+    },
+  };
+};
+
 export default Post;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import styles from "/styles/Home.module.scss";
 import fs from "fs";
-
 
 const BlogHome = ({ slugs }) => {
   return (
