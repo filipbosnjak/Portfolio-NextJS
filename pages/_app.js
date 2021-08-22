2d5d7b66683843e6187c2ba040efc24acfa64ba5
filipbosnjak <filipo.bosnjak@gmail.com>
2021-08-22 11:46:54 +0200

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
 //Trough here we import global styles
 
 import "../styles/globals.css";
+import Script from "next/script";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
 const NextApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload'>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+  `}
+      </Script>
+      <Component {...pageProps} />{" "}
+    </>
+  );
 };
 
 export default NextApp;
