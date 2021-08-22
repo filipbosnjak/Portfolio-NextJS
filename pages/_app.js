9c1924fd304394aae373a039c84be6cac6fa4529
filipbosnjak <filipo.bosnjak@gmail.com>
2021-08-22 18:31:05 +0200

    debugger

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,7 @@ const NextApp = ({ Component, pageProps }) => {
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+      debugger;
   `}
       </Script>
       <Component {...pageProps} />{" "}
